9070a2f1da86e6f60524d9f48b06206cda4b00b7
Anibal Sanchez <anibalnuma@gmail.com>
2025-10-15 17:10:48 -0400

    Refactorization

--- Application/Helpers/PasswordHelper.cs
+++ Application/Helpers/PasswordHelper.cs
@@ -3,23 +3,12 @@ namespace Application.Helpers;
 public static class PasswordHelper
 {
    
-    /// <summary>
-    /// Hashes a plain-text password using bcrypt.
-    /// </summary>
-    /// <param name="password"></param>
-    /// <returns></returns>
-    public static string HashPassword(string password)
+   public static string HashPassword(string password)
     {
         return BCrypt.Net.BCrypt.HashPassword(password);
     }
 
  
-    /// <summary>
-    /// Verifies a plain-text password against a hashed password.
-    /// </summary>
-    /// <param name="password"></param>
-    /// <param name="hashedPassword"></param>
-    /// <returns></returns>
     public static bool VerifyPassword(string password, string hashedPassword)
     {
         return BCrypt.Net.BCrypt.Verify(password, hashedPassword);
